0c3aefcb37429d5172c110ab0d59d4e6e5173ebc
187-Shogun <187shogun@gmail.com>
2022-02-06 22:30:38 -0600

    Completed building dataset loaders and began building training phase.

--- MalariaClassification/malaria_classifier.py
+++ MalariaClassification/malaria_classifier.py
@@ -11,14 +11,33 @@ Description: <Some description>
 """
 
 from tqdm import tqdm
+from pytz import timezone
+from datetime import datetime
 from tensorflow.keras.utils import image_dataset_from_directory
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import layers
+from tensorflow.keras.losses import BinaryCrossentropy
+from tensorflow.keras.callbacks import TensorBoard
+import tensorflow as tf
 import os
 import shutil
 import random
 
 
+AUTOTUNE = tf.data.AUTOTUNE
 TRAIN_DIR = os.path.join(os.getcwd(), 'Datasets', 'Train')
 TEST_DIR = os.path.join(os.getcwd(), 'Datasets', 'Test')
+LOGS_DIR = os.path.join(os.getcwd(), 'Logs')
+BATCH_SIZE = 32
+IM_HEIGHT = 200
+IM_WIDTH = 200
+EPOCHS = 10
+
+
+def get_logs_dir():
+    ts = datetime.now(timezone('America/Costa_Rica'))
+    run_id = ts.strftime("run_%Y%m%d-%H%M%S")
+    return os.path.join(LOGS_DIR, run_id)
 
 
 def reset_folders():
@@ -70,19 +89,54 @@ def split_raw_dataset(test_split: float = 0.2, reset: bool = False):
             shutil.copy(source, destination)
 
 
-def get_train_dataloader(batch_size: int, im_height: int, im_width: int, validation: float = 0.2):
+def get_dataset(batch_size: int, im_height: int, im_width: int, subset: str = 'training', validation: float = 0.2):
     return image_dataset_from_directory(
         TRAIN_DIR,
         validation_split=validation,
-        subset="training",
+        subset=subset,
         seed=420,
         image_size=(im_height, im_width),
-        batch_size=batch_size
+        batch_size=batch_size,
     )
 
 
+def get_baseline_network():
+    model = Sequential([
+        layers.Flatten(input_shape=(IM_HEIGHT, IM_WIDTH, 3)),
+        layers.Dense(100, activation='relu'),
+        layers.Dense(1, activation='sigmoid')
+    ])
+    model.compile(
+        optimizer='adam',
+        loss=BinaryCrossentropy(),
+        metrics='accuracy'
+    )
+    return model
+
+
 def main():
+    # Prepare the raw data to be loaded:
     split_raw_dataset()
+
+    # Load data into a generator:
+    train_ds = get_dataset(BATCH_SIZE, IM_HEIGHT, IM_WIDTH)
+    val_ds = get_dataset(BATCH_SIZE, IM_HEIGHT, IM_WIDTH, subset='validation')
+
+    # Normalize data prior training:
+    normalization_layer = layers.Rescaling(1./255)
+    train_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+
+    # Cache the datasets:
+    train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+    val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+    # Get some callbacks to use during training:
+    tb_logs = TensorBoard(get_logs_dir())
+
+    # Start training:
+    bs_model = get_baseline_network()
+    history = bs_model.fit(train_ds, validation_data=val_ds, epochs=EPOCHS, callbacks=[tb_logs])
+    print(history)
     return {}
 
 
