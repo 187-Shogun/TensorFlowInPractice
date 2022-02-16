231d4dbfd69cc1135c6e45808fb77f2311567c83
187-Shogun <187shogun@gmail.com>
2022-02-16 14:50:00 -0600

    Added func to train pretrained model.

--- Cars196Classification/cars_classifier.py
+++ Cars196Classification/cars_classifier.py
@@ -6,20 +6,16 @@ Created on: 2/9/2022
 Author: 187-Shogun
 
 Encoding: UTF-8
-
-
-
 """
 
 
-from matplotlib import pyplot as plt
 from tensorflow.keras import Model
 from tensorflow.keras import layers
 from tensorflow.keras import models
 from tensorflow.keras import optimizers
 from tensorflow.keras import losses
 from tensorflow.keras import callbacks
-from tensorflow.keras.applications import vgg16
+from tensorflow.keras import applications
 from tensorflow import image
 from pytz import timezone
 from datetime import datetime
@@ -28,16 +24,16 @@ import tensorflow as tf
 import os
 
 
-plt.style.use('dark_background')
+# Global variables:
 AUTOTUNE = tf.data.AUTOTUNE
 LOGS_DIR = os.path.join(os.getcwd(), 'Logs')
 CM_DIR = os.path.join(os.getcwd(), 'ConfusionMatrixes')
 MODELS_DIR = os.path.join(os.getcwd(), 'Models')
-BATCH_SIZE = 32
+BATCH_SIZE = 16
 IM_HEIGHT = 224
 IM_WIDTH = 224
-EPOCHS = 10000
-PATIENCE = 50
+EPOCHS = 100
+PATIENCE = 10
 RANDOM_SEED = 420
 
 
@@ -53,7 +49,6 @@ def download_dataset() -> tuple:
     train, info = tfds.load(
         'cars196',
         shuffle_files=True,
-        batch_size=BATCH_SIZE,
         with_info=True,
         as_supervised=True,
         split=['train']
@@ -61,16 +56,14 @@ def download_dataset() -> tuple:
     val = tfds.load(
         'cars196',
         shuffle_files=True,
-        batch_size=BATCH_SIZE,
         as_supervised=True,
-        split=['test[50%:]']
+        split=['test[75%:]']
     ),
     test = tfds.load(
         'cars196',
         shuffle_files=True,
-        batch_size=BATCH_SIZE,
         as_supervised=True,
-        split=['test[:50%]']
+        split=['test[:75%]']
     )
     return train[0], val[0][0], test[0], info
 
@@ -116,13 +109,17 @@ def get_custom_cnn(num_classes: int):
     return model
 
 
-def get_pretrained_vgg16(n_classes: int):
-    """ Load a pretrained VGG16 model and add a custom laer on top. """
-    base_model = vgg16.VGG16(weights='imagenet', include_top=False)
+def get_pretrained_network(n_classes: int):
+    """ Load a pretrained model and add a custom laer on top. """
+    # Import pretrained lower layers:
+    input_shape = (IM_HEIGHT, IM_WIDTH, 3)
+    base_model = applications.xception.Xception(weights='imagenet', include_top=False, input_shape=input_shape)
     for layer in base_model.layers:
         layer.trainable = False
-    ga_layer = layers.GlobalAveragePooling2D()(base_model.output)
-    out_layer = layers.Dense(n_classes, activation='softmax')(ga_layer)
+
+    # Add custom top layers:
+    pooling_layer = layers.GlobalAveragePooling2D()(base_model.output)
+    out_layer = layers.Dense(n_classes, activation='softmax')(pooling_layer)
     model = Model(inputs=base_model.inputs, outputs=out_layer)
     model.compile(
         optimizer=optimizers.SGD(momentum=0.9, nesterov=True),
@@ -132,28 +129,76 @@ def get_pretrained_vgg16(n_classes: int):
     return model
 
 
-def main():
-    """ Run script. """
-    X_train, X_val, X_test, info = download_dataset()
-    label_names = info.features['label'].names
+def train_pretrained_network(ds_info, training_ds, validation_ds, pretrain_rounds=10):
+    """ Build a model from a pretrained one. Freeze the bottom layers and train the top layers
+    for n epochs. Then, unfreeze the bottom layers, adjust the learning rate down and train the
+    model one more time. """
+    # Import pretrained lower layers:
+    label_names = ds_info.features['label'].names
+    input_shape = (IM_HEIGHT, IM_WIDTH, 3)
+    base_model = applications.resnet_v2.ResNet101V2(weights='imagenet', include_top=False, input_shape=input_shape)
+    for layer in base_model.layers:
+        layer.trainable = False
 
-    # Normalize data prior training:
-    normalization_layer = layers.Rescaling(1./255.)
-    X_train = X_train.map(lambda x, y: (image.resize(x, [IM_HEIGHT, IM_WIDTH]), y))
-    X_train = X_train.map(lambda x, y: (normalization_layer(x), y))
-    X_train = X_train.prefetch(buffer_size=AUTOTUNE)
-    X_val = X_val.map(lambda x, y: (image.resize(x, [IM_HEIGHT, IM_WIDTH]), y))
-    X_val = X_val.map(lambda x, y: (normalization_layer(x), y))
-    X_val = X_val.prefetch(buffer_size=AUTOTUNE)
+    # Add custom top layers:
+    pooling_layer = layers.GlobalAveragePooling2D()(base_model.output)
+    dropout_layer = layers.Dropout(0.25)(pooling_layer)
+    out_layer = layers.Dense(len(label_names), activation='softmax')(dropout_layer)
+    model = Model(inputs=base_model.inputs, outputs=out_layer, name='ResNet50V2-CNN')
+    model.compile(
+        optimizer=optimizers.SGD(momentum=0.9, nesterov=True),
+        loss=losses.SparseCategoricalCrossentropy(),
+        metrics='accuracy'
+    )
 
-    # Start training a single model:
-    model = get_pretrained_vgg16(len(label_names))
+    # Start pretraining:
     version_name = get_model_version_name(model.name)
     tb_logs = callbacks.TensorBoard(os.path.join(LOGS_DIR, version_name))
-    early_stop = callbacks.EarlyStopping(patience=PATIENCE, restore_best_weights=True)
-    # lr_scheduler = callbacks.ReduceLROnPlateau(factor=.5, patience=5)
-    model.fit(X_train, validation_data=X_val, epochs=EPOCHS, callbacks=[tb_logs, early_stop])
+    model.fit(training_ds, validation_data=validation_ds, epochs=pretrain_rounds, callbacks=[tb_logs])
     model.save(os.path.join(MODELS_DIR, f"{version_name}.h5"))
+
+    # Unfreeze layers and train the entire network:
+    for layer in model.layers:
+        layer.trainable = True
+    model.compile(
+        optimizer=optimizers.SGD(learning_rate=0.001, momentum=0.9, nesterov=True),
+        loss=losses.SparseCategoricalCrossentropy(),
+        metrics='accuracy'
+    )
+    early_stop = callbacks.EarlyStopping(patience=PATIENCE, restore_best_weights=True)
+    lr_scheduler = callbacks.ReduceLROnPlateau(factor=.5, patience=int(PATIENCE/2))
+    model.fit(training_ds, validation_data=validation_ds, epochs=EPOCHS, callbacks=[tb_logs, early_stop, lr_scheduler])
+    model.save(os.path.join(MODELS_DIR, f"{version_name}.h5"))
+    return model
+
+
+def get_preprocessing_layer():
+    """ Stack preprocessing layers together. """
+    return models.Sequential([
+        layers.RandomFlip(),
+        layers.RandomRotation(0.3)
+        # layers.RandomContrast(0.3)
+    ])
+
+
+def main():
+    """ Run script. """
+    # Normalize data prior training:
+    X_train, X_val, X_test, info = download_dataset()
+    method = image.ResizeMethod.NEAREST_NEIGHBOR
+    normalization_layer = layers.Rescaling(1./255.)
+    preprocessing_layer = get_preprocessing_layer()
+    X_train = X_train.map(lambda x, y: (image.resize(x, [IM_HEIGHT, IM_WIDTH], method=method), y))
+    X_train = X_train.map(lambda x, y: (normalization_layer(x), y))
+    X_train = X_train.map(lambda x, y: (preprocessing_layer(x), y))
+    X_train = X_train.batch(BATCH_SIZE).prefetch(buffer_size=AUTOTUNE)
+    X_val = X_val.map(lambda x, y: (image.resize(x, [IM_HEIGHT, IM_WIDTH], method=method), y))
+    X_val = X_val.map(lambda x, y: (normalization_layer(x), y))
+    X_val = X_val.batch(BATCH_SIZE).prefetch(buffer_size=AUTOTUNE)
+
+    # Start training a single model:
+    model = train_pretrained_network(info, X_train, X_val)
+    print(model.name)
     return {}
 
 
