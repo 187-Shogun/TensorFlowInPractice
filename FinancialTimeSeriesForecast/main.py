40280b588fc79224fbb368acae062c298d2e59fb
187-Shogun <187shogun@gmail.com>
2022-02-17 22:09:47 -0600

    Added predictions method and viz.

--- FinancialTimeSeriesForecast/main.py
+++ FinancialTimeSeriesForecast/main.py
@@ -26,6 +26,8 @@ import tensorflow as tf
 plt.style.use('dark_background')
 BATCH_SIZE = 64
 EPOCHS = 10
+SAMPLES = 30
+PREDICTIONS = 3
 
 
 def generate_sequences_from_df(dataframe: pd.DataFrame, seq_len: int = 30, predictions: int = 1) -> np.array:
@@ -35,12 +37,15 @@ def generate_sequences_from_df(dataframe: pd.DataFrame, seq_len: int = 30, predi
     scaler = MinMaxScaler()
     data = dataframe.close.values
     output = []
-    for i in range(len(data) - seq_len):
+    for i in range(len(data)):
         seq = data[i: (seq_len+i)+predictions]
-        seq = seq.reshape(-1, 1)
-        scaler.fit(seq)
-        seq = scaler.transform(seq)
-        output.append(seq)
+        if len(seq) == seq_len + predictions:
+            seq = seq.reshape(-1, 1)
+            scaler.fit(seq)
+            seq = scaler.transform(seq)
+            output.append(seq)
+        else:
+            break
     return np.array(output)
 
 
@@ -63,8 +68,8 @@ def build_lstm_model():
 def build_dummy_model():
     """ Build a simple neural network for a baseline evaluation. """
     model = models.Sequential([
-        layers.Flatten(input_shape=(30, 1)),
-        layers.Dense(1)
+        layers.Flatten(input_shape=(SAMPLES, 1)),
+        layers.Dense(PREDICTIONS)
     ])
     model.compile(
         optimizer=optimizers.Adam(learning_rate=0.1),
@@ -72,6 +77,18 @@ def build_dummy_model():
         metrics=[metrics.MeanAbsoluteError()]
     )
     return model
+
+
+def make_prediction(test_data, model):
+    next_ts = np.arange(SAMPLES, SAMPLES + PREDICTIONS)
+    n = np.random.randint(0, BATCH_SIZE)
+    test_seq, test_label = [x for x in test_data.as_numpy_iterator()][np.random.randint(0, len(test_data)-1)]
+    y_pred = model.predict(test_seq[n].reshape((1, SAMPLES, 1))).flatten()
+    true_seq = np.concatenate([test_seq[n].flatten(), test_label[n].flatten()])
+    plt.figure(figsize=(13, 8))
+    plt.plot(true_seq)
+    plt.plot(next_ts, y_pred, 'r--')
+    plt.show()
 
 
 def main():
@@ -84,9 +101,9 @@ def main():
     df = df[['date_formatted', 'close']]
 
     # Scale data points between 0 & 1 and create datasets for training:
-    sequences = generate_sequences_from_df(df)
+    sequences = generate_sequences_from_df(df, predictions=PREDICTIONS, seq_len=SAMPLES)
     ds = tf.data.Dataset.from_tensor_slices(sequences)
-    ds = ds.map(lambda x: (x[:30], x[-1]))
+    ds = ds.map(lambda x: (x[:SAMPLES], x[-PREDICTIONS:]))
     total_seqs = len(ds)
     train_seqs = int(total_seqs * 0.8)
     X_train = ds.take(train_seqs).batch(BATCH_SIZE)
@@ -95,16 +112,9 @@ def main():
     # Train a baseline model:
     baseline_model = build_dummy_model()
     baseline_model.fit(X_train, epochs=EPOCHS)
-    predictions = baseline_model.predict(X_test)
 
     # Test the model:
-    n = np.random.randint(0, BATCH_SIZE)
-    test_seq, test_label = [x for x in X_test.as_numpy_iterator()][0]
-    plt.figure(figsize=(13, 8))
-    plt.plot(test_seq[n])
-    plt.plot(30, test_label[n], 'gx')
-    plt.plot(30, baseline_model.predict(test_seq[n].reshape((1, 30, 1))), 'ro')
-    plt.show()
+    make_prediction(X_test, baseline_model)
     return {}
 
 
